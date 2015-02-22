1540265dc924ed34df8372e44af9096a44654fd7
Gonzalo <gonzalovazquezs@gmail.com>
2015-02-22 13:36:20 +0100

    01- Modelado

--- Core/admin.py
+++ Core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from Core.models import *
 
-# Register your models here.
+admin.site.register(Comunidades)
+admin.site.register(Provincias)
+admin.site.register(Municipios)
+admin.site.register(Perfil)
+admin.site.register(Rol)
+admin.site.register(Nivel)
+admin.site.register(Club)
+admin.site.register(Sociedad)
+admin.site.register(Pista)
+admin.site.register(Deporte)
+admin.site.register(Partido)
--- Core/models.py
+++ Core/models.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -21,13 +22,57 @@ class Municipios(models.Model):
 	def __unicode__(self):
 		return self.municipio
 
-class Roles(models.Model):
+class Rol(models.Model):
     tipo = models.CharField(max_length=50, verbose_name="Rol")
+    def __unicode__(self):
+		return self.tipo
+
+class Nivel(models.Model):
+    nivel = models.CharField(max_length=50, verbose_name="Nivel")
+    def __unicode__(self):
+		return self.nivel
+
+class Deporte(models.Model):
+    deporte = models.CharField(max_length=50, verbose_name="Deporte")
+    def __unicode__(self):
+		return self.deporte
+
+class Sociedad(models.Model):
+    nombre = models.CharField(max_length=50, verbose_name="Nombre")
+    cif = models.CharField(max_length=50, verbose_name="CIF")
+    def __unicode__(self):
+		return self.nombre
+
+class Club(models.Model):
+    nombre = models.CharField(max_length=50, verbose_name="Nombre")
+    municipio = models.ForeignKey(Municipios, related_name='municipio_club', blank=True, null=True, on_delete=models.SET_NULL)
+    descripcion = models.TextField(max_length=400, verbose_name="Descripción")
+    imagen = models.ImageField(upload_to='Imagenes', verbose_name='Imagen', blank=True)
+    sociedad = models.ForeignKey(Sociedad, unique=True, related_name="club_sociedad")
+    def __unicode__(self):
+		return self.nombre
+
+class Pista(models.Model):
+    nombre = models.CharField(max_length=50, verbose_name="Nombre")
+    club = models.ForeignKey(Club, related_name="pista_club", unique=True)
+    deporte = models.ForeignKey(Deporte, related_name="pista_deporte", unique=True)
+    def __unicode__(self):
+		return self.nomre + " (" +self.club.nombre + ")"
 
 class Perfil(models.Model):
-    user = models.ForeignKey(User, unique=True, related_name='perfil')
-    rol = models.ForeignKey(Roles, unique=True, related_name="rol")
+    user = models.ForeignKey(User, unique=True, related_name='perfil_user')
+    rol = models.ForeignKey(Rol, unique=True, related_name="perfil_rol")
+    nivel = models.ForeignKey(Nivel, unique=True, related_name="perfil_nivel")
     imagen = models.ImageField(upload_to='Imagenes', verbose_name='Imagen', blank=True)
-    municipio = models.ForeignKey(Municipios, related_name='municipio_perfil', blank=True, null=True, on_delete=models.SET_NULL)
+    municipio = models.ForeignKey(Municipios, related_name='perfil_municipio', blank=True, null=True, on_delete=models.SET_NULL)
+    clubes = models.ManyToManyField(Club, related_name="perfil_clubes")
     def __unicode__(self):
-		return self.user.first_name
+		return self.user.first_name + " " + self.user.last_name
+
+class Partido(models.Model):
+    pista = models.ForeignKey(Pista, unique=True, related_name="pista", verbose_name="Pista")
+    hora = models.TimeField(auto_now=False, verbose_name="Hora")
+    fecha = models.DateField(auto_now=False, verbose_name="Fecha")
+    perfiles = models.ManyToManyField(Perfil, related_name="partido_perfiles")
+    def __unicode__(self):
+		return "Pista: " + self.pista.nombre + ", Fecha: " + self.fecha + ", Hora:" + self.hora
