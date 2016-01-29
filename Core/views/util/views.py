a001c95e06a73eadda08e8b337ee1fcdc79696de
Gonzalo <gonzalovazquezs@gmail.com>
2016-01-29 19:57:21 +0100

    Modificación emails con html

--- Core/views/util/views.py
+++ Core/views/util/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.conf import settings
 import json
 import logging
+from django.core.mail import EmailMultiAlternatives
 
 #Instancia del log
 logger = logging.getLogger(__name__)
@@ -15,12 +16,18 @@ def send_email(request):
     error = ""
     if subject and message and from_email:
         try:
-            texto = "Email enviado desde Sport Click: \n"
-            texto += "De: " + from_email + "\n"
-            texto += "Titulo: " + subject + "\n"
-            texto += "Cuerpo: " + message + "\n"
-            send_mail(subject, texto, settings.EMAIL_HOST_USER, [settings.EMAIL_HOST_USER])
+            texto = "<h5>Email enviado desde Sport Click:</h5> \n"
+            texto += "<h5>De: </h5>" + from_email + "\n"
+            texto += "<h5>Titulo: </h5>" + subject + "\n"
+            texto += "<h5>Cuerpo: </h5>" + message + "\n"
+            #send_mail(subject, texto, settings.EMAIL_HOST_USER, [settings.EMAIL_HOST_USER])
+            msg = EmailMultiAlternatives(subject,'' , settings.EMAIL_HOST_USER, [settings.EMAIL_HOST_USER])
+            msg.attach_alternative(texto, "text/html")
+            msg.send()
         except BadHeaderError, e:
+            logger.debug("util/views - Método send_email." + e)
+            error = "Ha habido un error en el proceso. Disculpe las molestias"
+        except Exception, e:
             logger.debug("util/views - Método send_email." + e)
             error = "Ha habido un error en el proceso. Disculpe las molestias"
     else:
